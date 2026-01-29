f543b97db8cb95f8bc094dbca88ba4ed58cafe85
jayantkr <jayant0899@gmail.com>
2026-01-29 23:32:23 +0530

    docs: Comment out Twitter metadata configuration in `app/layout.tsx`.

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,13 +43,13 @@ export const metadata: Metadata = {
       },
     ],
   },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [siteConfig.ogImage],
-    creator: `@${userConfig.twitter.split("/").pop()}`,
-  },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: siteConfig.name,
+  //   description: siteConfig.description,
+  //   images: [siteConfig.ogImage],
+  //   creator: `@${userConfig.twitter.split("/").pop()}`,
+  // },
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
